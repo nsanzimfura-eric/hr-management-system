336cf4f91b8eb9564c74e49e6ec089ba0b48b06f
nsanzimfura-eric <erickykress1@gmail.com>
2024-03-28 12:56:43 +0200

    Fixed meeting notfication on dm deveices'

--- src/pages/dashboard/components/upComingMeetings/upComingMeetings.tsx
+++ src/pages/dashboard/components/upComingMeetings/upComingMeetings.tsx
@@ -22,29 +22,28 @@ const UpComingMeetings = (props: upComingMeetingsProps) => {
         setClickedMeetingMeeting(id);
     };
 
-    return (<div className={styles.upComingMeetings}>
-        <div className='d-flex titleBox'>
-            <Typography className='title' variant='subtitle1'>Upcoming Meetings</Typography>
-            <ButtonIcon url="/svgs/addBlue.svg" onClick={handleClickAddMeeting} />
-        </div>
-        <div className='meetingsWrapper'>
-            {meetingsData.map((data: MeetingsInterface, index: number) => {
-                return (
-                    <div key={index} className="meetingWrapper">
-                        <Typography>{data.time}</Typography>
-                        <div className='meetingActivities'>
-                            {data.activity?.map((meeting, idx: number) => {
-                                return <MeetingNotification key={idx} meeting={meeting} onClick={handleClickedMeeting} index={idx} clickedIndex={clickedMeetingMeeting} />
-                            })}
+    return (
+        <div className={showMeetings ? styles.upComingMeetings : styles.transformedRight}>
+            <div className='d-flex titleBox'>
+                <Typography className='title' variant='subtitle1'>Upcoming Meetings</Typography>
+                <ButtonIcon url="/svgs/addBlue.svg" onClick={handleClickAddMeeting} />
+            </div>
+            <div className='meetingsWrapper'>
+                {meetingsData.map((data: MeetingsInterface, index: number) => {
+                    return (
+                        <div key={index} className="meetingWrapper">
+                            <Typography>{data.time}</Typography>
+                            <div className='meetingActivities'>
+                                {data.activity?.map((meeting, idx: number) => {
+                                    return <MeetingNotification key={idx} meeting={meeting} onClick={handleClickedMeeting} index={idx} clickedIndex={clickedMeetingMeeting} />
+                                })}
+                            </div>
                         </div>
-                    </div>
-                )
-            })}
-
-
+                    )
+                })}
+            </div>
         </div>
-
-    </div>);
+    );
 };
 
 export default UpComingMeetings;
