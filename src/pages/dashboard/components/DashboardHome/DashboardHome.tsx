3e69edf4d1904f80a7ff75dd1a229737c400911c
nsanzimfura-eric <erickykress1@gmail.com>
2024-03-28 20:20:49 +0200

    Design header middle part in the gome dashboard

--- src/pages/dashboard/components/DashboardHome/DashboardHome.tsx
+++ src/pages/dashboard/components/DashboardHome/DashboardHome.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styles from "./dashboardHome.module.scss";
 import UpComingMeetings from "../../../../components/upComingMeetings/upComingMeetings";
 import ButtonIcon from "../../../../components/ButtonIcon/ButtonIcon";
+import { Typography } from "@mui/material";
 
 
 const DashboardHome = () => {
@@ -13,8 +14,26 @@ const DashboardHome = () => {
 
   return (
     <div className={styles.dashboardHome}>
-      <div className="contentMiddleWrapper  bg-warning">
-        test wrapper
+      <div className="contentMiddleWrapper">
+        {/* header */}
+        <div className="w-full d-flex justify-content-between align-items-center wrapperHeader">
+          <Typography className='title' variant='subtitle1'>OverView</Typography>
+          <div className="btns d-flex  align-items-center ms-auto">
+            <button className="btn btn-primary">
+              <img src="/svgs/addIcon_White.svg" alt="Add Icon" />
+              <span>Add Candidate</span>
+            </button>
+            <button className="btn btn-primary">
+              <img src="/svgs/addIcon_White.svg" alt="Add Icon" />
+              <span>Add Job</span>
+            </button>
+          </div>
+        </div>
+        {/* Overview body */}
+        <div className="overViewBody">
+
+        </div>
+
       </div>
       <UpComingMeetings showMeetings={showMeetings} />
       <div className="buttonToggleMeetings">
