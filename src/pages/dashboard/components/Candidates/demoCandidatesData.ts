04b1227aae288f5739306311a7c23a828cf76ed8
nsanzimfura-eric <erickykress1@gmail.com>
2024-03-29 02:16:09 +0200

    create candidate dummy data

---
+++ src/pages/dashboard/components/Candidates/demoCandidatesData.ts
@@ -0,0 +1,102 @@
+export const demoCandidatesData = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "John.Doe@gmail.com",
+    phone: "+250 88 878 456",
+    address: "KN 109 Main Street",
+    linkedin: "https://www.linkedin.com/in/nsanzimfura-eric",
+    website: "https://nsanzimfura.web.app/",
+    twitter: "https://nsanzimfura.web.app/",
+    files: [
+      { name: "Cover_letter.pdf", time: "2d ago" },
+      { name: "My_resume.pdf", time: "2d ago" },
+      { name: "Oct_payslip.pdf", time: "2d ago" },
+      { name: "Oct_payslip.pdf", time: "2d ago" },
+    ],
+    skills: [
+      {
+        name: "Qualifications and skills match",
+        good: true,
+      },
+      {
+        name: "Experience Rlevance",
+        good: true,
+      },
+      {
+        name: "Education",
+        good: true,
+      },
+      {
+        name: "Keywords Match",
+        good: false,
+      },
+      {
+        name: "Years of Experience",
+        good: true,
+      },
+      {
+        name: "Job Hopping",
+        good: false,
+      },
+      {
+        name: "Cultural Fit",
+        good: true,
+      },
+      {
+        name: "Interview Performance",
+        good: true,
+      },
+      {
+        name: "References",
+        good: false,
+      },
+      {
+        name: "Additional Factors",
+        good: true,
+      },
+    ],
+    experiences: `
+    <section>
+        <strong>Senior Data Analyst</strong>
+        <div>Alight <span>(May 2021 - Present)</span></div>
+        <strong>Responsible for;</strong>
+        <ul>
+            <li>
+                1. Data Exploration and Analysis: They perform exploratory data analysis to uncover insights, trends, and patterns in the data, often using statistical and visualisation techniques.
+            </li>
+            <li>
+                2. Data Cleaning and Preprocessing: Data analysts are responsible for cleaning and preparing raw renewable data to ensure its accuracy and reliability for analysis.
+            </li>
+            <li>
+                3. Reporting and Communication: Data analysts communicate their findings through reports, dashboards, and presentations to help stakeholders make informed decisions based on the data-driven insights.
+            </li>
+        </ul>
+    </section>
+    <section>
+        <strong>Junior Data Analyst</strong>
+        <div>Alight <span>(May 2020 - May 2021)</span></div>
+        <strong>Responsible for;</strong>
+        <ul>
+            <li>
+                1. Data Exploration and Analysis: They perform exploratory data analysis to uncover insights, trends, and patterns in the data, often using statistical and visualisation techniques.
+            </li>
+            <li>
+                2. Data Cleaning and Preprocessing: Data analysts are responsible for cleaning and preparing raw renewable data to ensure its accuracy and reliability for analysis.
+            </li>
+            <li>
+                3. Reporting and Communication: Data analysts communicate their findings through reports, dashboards, and presentations to help stakeholders make informed decisions based on the data-driven insights.
+            </li>
+        </ul>
+    </section>
+    
+    `,
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    email: "jane.smith@gmail.com",
+    phone: "+250 87 492 456",
+    address: "123 Main Street, New York, NY 10001",
+  },
+];
