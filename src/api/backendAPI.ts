185b650642029ee02bc6a9286966e7777d99e684
nsanzimfura-eric <erickykress1@gmail.com>
2024-03-31 17:46:48 +0200

    Define all backend routes

--- src/api/backendAPI.ts
+++ src/api/backendAPI.ts
@@ -1,6 +1,10 @@
-const BaseUrl = 'http://localhost:8080'
+const BaseUrl = import.meta.env.VITE_API_URL;
 
 export const backendAPI = {
-    login: `${BaseUrl}/login`,
-    signup: `${BaseUrl}/signup`,
-}
+  login: `${BaseUrl}/auth/login`,
+  signup: `${BaseUrl}/auth/signup`,
+  createJob: `${BaseUrl}/jobs/createJob`,
+  getAllJobs: `${BaseUrl}/jobs/listOfJobs`,
+  deleteJob: (jobId: string) => `${BaseUrl}/jobs/delete/${jobId}`,
+  applyToJob: (jobId: string) => `${BaseUrl}/candidates/apply/${jobId}`,
+};
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -41,7 +41,6 @@ const Login = () => {
                 {/*  form */}
                 <FormAuth />
               </div>
-
             </div>
           </div>
         </div>
